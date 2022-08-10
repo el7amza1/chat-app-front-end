86787b8804bd6ed22976fbdd20580512746dce12
el7amza1 <elhamza19932020@gmail.com>
2022-08-10 14:28:55 +0200

    create sing up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
+import {  Route, Routes } from "react-router-dom"
 import './App.css';
+import SignUp from "./pages/signUp";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" >
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
